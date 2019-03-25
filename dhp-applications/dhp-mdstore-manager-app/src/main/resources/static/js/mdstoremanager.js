b1200b6f467aef75d8ae7eafdbd150c80d37682f
Michele Artini <michele.artini@isti.cnr.it>
2019-03-25 11:54:18 +0100

    ui

--- dhp-applications/dhp-mdstore-manager-app/src/main/resources/static/js/mdstoremanager.js
+++ dhp-applications/dhp-mdstore-manager-app/src/main/resources/static/js/mdstoremanager.js
@@ -6,29 +6,30 @@ app.controller('mdstoreManagerController', function($scope, $http) {
 	$scope.forceVersionDelete = false;
 	
 	$scope.reload = function() {
-		$http.get('/mdstores/').success(function(data) {
+		$http.get('/mdstores/?' + $.now()).success(function(data) {
 			$scope.mdstores = data;
 		}).error(function() {
 			alert("error");
 		});
 	};
-	
-	$scope.listVersions = function(mdId, current) {
-		$scope.versions = [];
-		$http.get('/mdstores/mdstore/' + mdId + '/versions').success(function(data) {
-			angular.forEach(data, function(value, key) {
-				value.current = (value.id == current);
-			});
-			$scope.versions = data;
+		
+	$scope.newMdstore = function(format, layout, interpretation, dsName, dsId, apiId) {
+		var url = '/mdstores/new/' + encodeURIComponent(format) + '/' + encodeURIComponent(layout) + '/' + encodeURIComponent(interpretation);
+		if (dsName || dsId || apiId) {
+			url += '?dsName=' + encodeURIComponent(dsName) + '&dsId=' + encodeURIComponent(dsId) + '&apiId=' + encodeURIComponent(apiId);
+		}
+		$http.put(url).success(function(data) {
+			
+			$scope.reload();
 		}).error(function() {
 			alert("error");
 		});
 	};
- 		
+	 		
 	$scope.deleteMdstore = function(mdId) {
 		if (confirm("Are you sure ?")) {
 			$http.delete('/mdstores/mdstore/' + mdId).success(function(data) {
-				$scope.reload();
+				$scope.reload, 500;
 			}).error(function() {
 				alert("error");
 			});
@@ -37,19 +38,18 @@ app.controller('mdstoreManagerController', function($scope, $http) {
 
 	$scope.prepareVersion = function(mdId, currentVersion) {
 		$scope.versions = [];
-		$http.get('/mdstores/mdstore/' + mdId + '/newVersion').success(function(data) {
+		$http.get('/mdstores/mdstore/' + mdId + '/newVersion?' + $.now()).success(function(data) {
 			$scope.reload();
 			$scope.listVersions(mdId, currentVersion);
 		}).error(function() {
 			alert("error");
 		});
 	};
-	
-	
+
 	$scope.commitVersion = function(versionId) {
 		var size = parseInt(prompt("New Size", "0"));
 		if (size >= 0) {
-			$http.get("/mdstores/version/" + versionId + "/commit/" + size).success(function(data) {
+			$http.get("/mdstores/version/" + versionId + "/commit/" + size + '?' + $.now()).success(function(data) {
 				angular.forEach($scope.versions, function(value, key) {
 					if (value.id == versionId) {
 						value.current = true;
@@ -65,6 +65,18 @@ app.controller('mdstoreManagerController', function($scope, $http) {
 		}
 	};
 	
+	$scope.listVersions = function(mdId, current) {
+		$scope.versions = [];
+		$http.get('/mdstores/mdstore/' + mdId + '/versions?' + $.now()).success(function(data) {
+			angular.forEach(data, function(value, key) {
+				value.current = (value.id == current);
+			});
+			$scope.versions = data;
+		}).error(function() {
+			alert("error");
+		});
+	};
+	
 	$scope.deleteVersion = function(versionId, force) {
 		if (confirm("Are you sure ?")) {
 			var url = '/mdstores/version/' + versionId;
@@ -78,6 +90,7 @@ app.controller('mdstoreManagerController', function($scope, $http) {
 					}
 				});
 				$scope.versions = nv;
+				$scope.reload();
 			}).error(function() {
 				alert("error");
 			});
